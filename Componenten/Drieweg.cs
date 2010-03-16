ffbb45c201c46020fef50afd51fbc16a5a2e22cc
Pieter Kokx <pieter@kokx.nl>
2010-03-16 11:29:37 +0100

    Initiele componenten library.

---
+++ Componenten/Drieweg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fhict_proftaak3.Kruispunt
+{
+    public abstract class Drieweg : IKruispunt
+    {
+
+        public void addAuto(Auto auto, IKruispunt kruispunt);
+        public void removeAuto(Auto auto, IKruispunt kruispunt);
+
+        public void addKruispunt(IKruispunt kruispunt);
+    }
+}
---
+++ Componenten/IKruispunt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fhict_proftaak3.Kruispunt
+{
+    public interface IKruispunt
+    {
+        void addAuto(Auto auto, IKruispunt kruispunt);
+        void removeAuto(Auto auto, IKruispunt kruispunt);
+
+        void addKruispunt(IKruispunt kruispunt, int pos);
+    }
+}
