0454693576ad939c16f49a3c73d284284179bd8d
Bram Wijnands <bramlaar1@hotmail.com>
2010-03-18 09:55:55 +0100

    Form uitgebreid met keuzemogelijkheden

--- TFormRegeling1.Designer.cs
+++ TFormRegeling1.Designer.cs
@@ -29,11 +29,16 @@ namespace fhict_proftaak3
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 649);
+            this.button1.Location = new System.Drawing.Point(1097, 593);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -41,15 +46,87 @@ namespace fhict_proftaak3
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1097, 637);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run Simulatie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Type 1",
+            "Type 2",
+            "Type 3",
+            "Type 4"});
+            this.comboBox1.Location = new System.Drawing.Point(1051, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Type 1",
+            "Type 2",
+            "Type 3",
+            "Type 4"});
+            this.comboBox2.Location = new System.Drawing.Point(1051, 70);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Type 1",
+            "Type 2",
+            "Type 3",
+            "Type 4"});
+            this.comboBox3.Location = new System.Drawing.Point(1051, 114);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Type 1",
+            "Type 2",
+            "Type 3",
+            "Type 4"});
+            this.comboBox4.Location = new System.Drawing.Point(1051, 152);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 6;
+            // 
             // TFormRegeling1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 684);
+            this.ClientSize = new System.Drawing.Size(1184, 684);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "TFormRegeling1";
             this.Text = "TFormRegeling1";
             this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.comboBox3, 0);
+            this.Controls.SetChildIndex(this.comboBox4, 0);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +134,11 @@ namespace fhict_proftaak3
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
 
     }
 }
--- TFormRegeling1.cs
+++ TFormRegeling1.cs
@@ -10,6 +10,11 @@ namespace fhict_proftaak3
 {
     public partial class TFormRegeling1 : VerkeersComponentenLibrary.TFormRegeling
     {
+        TKP1 tkp1 = new TKP1();
+        TKP2 tkp2 = new TKP2();
+        TKP3 tkp3 = new TKP3();
+        TKP4 tkp4 = new TKP4();
+
         public TFormRegeling1()
         {
             InitializeComponent();
@@ -17,10 +22,47 @@ namespace fhict_proftaak3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.insertKruispuntForm(0, new TKP1(), 0, 0, 484, 339);
-            this.insertKruispuntForm(1, new TKP2(), 484, 0, 484, 339);
-            this.insertKruispuntForm(2, new TKP3(), 0, 339, 484, 339);
-            this.insertKruispuntForm(3, new TKP4(), 484, 339, 484, 339);
+            //Clear kruispunten
+            for (int i = 0; i < 4; i++)
+            {
+                this.removeKruispuntForm(i);
+            }
+            //Toevoegen Locatie 1
+            if (comboBox1.Text == "Type 1")
+                this.insertKruispuntForm(0, tkp1, 0, 0, 484, 339);
+            if (comboBox1.Text == "Type 2")
+                this.insertKruispuntForm(0, tkp2, 0, 0, 484, 339);
+            if (comboBox1.Text == "Type 3")
+                this.insertKruispuntForm(0, tkp3, 0, 0, 484, 339);
+            if (comboBox1.Text == "Type 4")
+                this.insertKruispuntForm(0, tkp4, 0, 0, 484, 339);
+            //Toevoegen Locatie 2
+            if (comboBox2.Text == "Type 1")
+                this.insertKruispuntForm(1, tkp1, 484, 0, 484, 339);
+            if (comboBox2.Text == "Type 2")
+                this.insertKruispuntForm(1, tkp2, 484, 0, 484, 339);
+            if (comboBox2.Text == "Type 3")
+                this.insertKruispuntForm(1, tkp3, 484, 0, 484, 339);
+            if (comboBox2.Text == "Type 4")
+                this.insertKruispuntForm(1, tkp4, 484, 0, 484, 339);
+            //Toevoegen Locatie 3
+            if (comboBox3.Text == "Type 1")
+                this.insertKruispuntForm(2, tkp1, 0, 339, 484, 339);
+            if (comboBox3.Text == "Type 2")
+                this.insertKruispuntForm(2, tkp2, 0, 339, 484, 339);
+            if (comboBox3.Text == "Type 3")
+                this.insertKruispuntForm(2, tkp3, 0, 339, 484, 339);
+            if (comboBox3.Text == "Type 4")
+                this.insertKruispuntForm(2, tkp4, 0, 339, 484, 339);
+
+            //this.insertKruispuntForm(0, tkp1, 0, 0, 484, 339);
+            //this.insertKruispuntForm(1, tkp2, 484, 0, 484, 339);
+            //this.insertKruispuntForm(2, tkp3, 0, 339, 484, 339);
+            //this.insertKruispuntForm(3, tkp4, 484, 339, 484, 339);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
