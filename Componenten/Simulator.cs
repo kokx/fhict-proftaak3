2223d6d558def9a713dd33afa9d578c4a6c91d7f
Pieter Kokx <pieter@kokx.nl>
2010-03-23 13:58:50 +0100

    Kruispunten toegevoegd in simulator.

--- Componenten/Simulator.cs
+++ Componenten/Simulator.cs
@@ -8,13 +8,38 @@ namespace fhict_proftaak3.Componenten
     {
 
         protected int ticks;
+        public int Ticks
+        {
+            get { return this.ticks; }
+        }
 
+        /// <summary>
+        /// PreSimulate event hook
+        /// </summary>
         public event EventHandler preSimulate;
+        /// <summary>
+        /// PostSimulate event hook
+        /// </summary>
         public event EventHandler postSimulate;
 
-        public Simulator()
+        protected IKruispunt[] kruispunten;
+        public IKruispunt[] Kruispunten
         {
+            get { return this.kruispunten; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Simulator(int maxKruispunten)
+        {
+            kruispunten = new IKruispunt[maxKruispunten];
             ticks = 0;
+        }
+
+        public void Set(IKruispunt kruispunt, int pos)
+        {
+            kruispunten[pos] = kruispunt;
         }
 
         public void simulate(int ticks)
