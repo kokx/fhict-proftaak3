06bc259420a28c559915a66ab4c3e6d7e5fc959a
Pieter Kokx <pieter@kokx.nl>
2010-03-25 10:22:35 +0100

    Direction enum toegevoegd.

---
+++ Componenten/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fhict_proftaak3.Componenten
+{
+    enum Direction
+    {
+        EAST,
+        WEST,
+        NORTH,
+        SOUTH
+    }
+}
--- Componenten/Kruispunten/Type4.cs
+++ Componenten/Kruispunten/Type4.cs
@@ -6,5 +6,10 @@ namespace fhict_proftaak3.Componenten.Kruispunten
 {
     class Type4 : Drieweg
     {
+
+        public Type4()
+        {
+            wachtrijen = new KruispuntWachtrij[3];
+        }
     }
 }
